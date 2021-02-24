3174f01072ef8cecfef0afdeb36ca23422df1759
Poshak Jaiswal <poshak@greytip.com>
2021-02-25 02:13:17 +0530

    dummy
    dummy

--- EPIJudge/epi_judge_python/count_bits.py
+++ EPIJudge/epi_judge_python/count_bits.py
@@ -2,10 +2,10 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here. Author : Poshak Jaiswal
+
     num_bits = 0
     while x:
-        num_bits =  num_bits + (x & 1)
+        num_bits = num_bits + (x & 1)
         x >>= 1
     return num_bits
 
