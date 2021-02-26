d6bb980416ab2785e3799bbfe27038d3c726ec45
Poshak Jaiswal <poshak@greytip.com>
2021-02-26 15:25:06 +0530

    parity

--- EPIJudge/epi_judge_python/parity.py
+++ EPIJudge/epi_judge_python/parity.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
+"""
+The parity of a binary word is 1 if the number of 1s in the word is odd; otherwise, it is 0. For
+example, the parity of 1011 is 1, and the parity of 10001000 is 0. Parity checks are used to detect
+single bit errors in data storage and communication. It is fairly straightforward to write code that
+computes the parity of a single 64-bit word.
+"""
+
 
 def parity(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    total_set_bits = 0
+    while x:
+        total_set_bits += (x & 1)
+        x = x >> 1
+
+    return total_set_bits % 2
 
 
 if __name__ == '__main__':
--- EPIJudge/epi_judge_python/reverse_bits.py
+++ EPIJudge/epi_judge_python/reverse_bits.py
@@ -3,6 +3,7 @@ from test_framework import generic_test
 
 def reverse_bits(x: int) -> int:
     # TODO - you fill in here.
+
     return 0
 
 
