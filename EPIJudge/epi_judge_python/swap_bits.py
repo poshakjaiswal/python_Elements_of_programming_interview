32cee18aff8cbe1b874216304b965ae5c5d6ee99
Poshak Jaiswal <poshak@greytip.com>
2021-02-27 03:11:17 +0530

    Added swap bits

--- EPIJudge/epi_judge_python/swap_bits.py
+++ EPIJudge/epi_judge_python/swap_bits.py
@@ -2,11 +2,16 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
+    # firstly check if the bits are just the same
+    if (x >> i & 1) != (x >> j & 1):
+        bit_mask = 1 << i | 1 << j
+        # create a new bit mask
+        x = x ^ bit_mask
+
     # TODO - you fill in here.
-    return 0
+    return x
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('swap_bits.py', 'swap_bits.tsv',
-                                       swap_bits))
+    # print(swap_bits(8, 0, 3))
+    exit(generic_test.generic_test_main('swap_bits.py', 'swap_bits.tsv', swap_bits))
