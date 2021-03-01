d1526bcd21c989f17b38badda61cbda1c83f10d3
Poshak Jaiswal <poshak@greytip.com>
2021-03-01 23:25:53 +0530

    multiply

---
+++ EPIJudge/epi_judge_python/Custom_Not_Present/multiply.py
@@ -0,0 +1,24 @@
+def multiply(x, y):
+    def add(a, b):
+        running_sum, carryin, k, temp_a, temp_b = 0, 0, 1, a, b
+        while temp_a or temp_b:
+            ak, bk = a & k, b & k
+            carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
+            running_sum |= ak ^ bk ^ carryin
+            carryin, k, temp_a, temp_b = (carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
+        return running_sum | carryin
+
+    running_sum = 0
+
+    while x:
+        if x & 1:
+            running_sum = add(running_sum, y)
+        x = x >> 1
+        y = y << 1
+
+    return running_sum
+
+
+if __name__ == '__main__':
+    first, second = 13, 9
+    print(multiply(first, second))
